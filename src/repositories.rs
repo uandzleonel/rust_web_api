af01af91243aecc47dce0a46bb7927a54028c6a3
Uanderson Leonel <dev.uandz@gmail.com>
2024-04-18 07:21:02 -0300

    Add Crate Repository

--- src/repositories.rs
+++ src/repositories.rs
@@ -1,7 +1,8 @@
 use diesel::{ExpressionMethods, QueryDsl, QueryResult};
 use diesel_async::{ AsyncPgConnection, RunQueryDsl };
 
-use crate::{models::{NewRustacean, Rustacean}, schema::{crates::table, rustaceans}};
+use crate::schema::*;
+use crate::models::*;
 
 pub struct RustaceanRepository;
 
@@ -33,5 +34,41 @@ impl RustaceanRepository {
 
   pub async fn delete(conn: &mut AsyncPgConnection, id: i32) -> QueryResult<usize> {
     diesel::delete(rustaceans::table.find(id)).execute(conn).await
+  }
+}
+
+pub struct CrateRepository;
+
+impl CrateRepository {
+  pub async fn find(conn: &mut AsyncPgConnection, id: i32) -> QueryResult<Crate> {
+    crates::table.find(id).get_result(conn).await
+  }
+
+  pub async fn find_many(conn: &mut AsyncPgConnection, limit: i64) -> QueryResult<Vec<Crate>> {
+    crates::table.limit(limit).get_results(conn).await
+  }
+
+  pub async fn create(conn: &mut AsyncPgConnection, new_crate: NewCrate) -> QueryResult<Crate> {
+    diesel::insert_into(crates::table)
+      .values(new_crate)
+      .get_result(conn)
+      .await
+  }
+
+  pub async fn update(conn: &mut AsyncPgConnection, a_crate: Crate) -> QueryResult<Crate> {
+    diesel::update(crates::table.find(a_crate.id))
+      .set((
+        crates::rustacean_id.eq(a_crate.rustacean_id),
+        crates::code.eq(a_crate.code),
+        crates::name.eq(a_crate.name),
+        crates::version.eq(a_crate.version),
+        crates::description.eq(a_crate.description),
+      ))
+      .get_result(conn)
+      .await
+  }
+  
+  pub async fn delete(conn: &mut AsyncPgConnection, id: i32) -> QueryResult<usize> {
+    diesel::delete(crates::table.find(id)).execute(conn).await
   }
 }
